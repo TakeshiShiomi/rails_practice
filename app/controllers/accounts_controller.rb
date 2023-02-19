2a8ee0e2e4d2fb504bcc2d3f6b0f63c731b0768f
USER <user@USERnoMacBook-Air-2.local>
2023-02-19 17:41:23 +0900

    Finish user signup

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,25 @@
 class AccountsController < ApplicationController
   def new
+    @account = Account.new
   end
+
+  def create
+    @account = Account.new(account_params)   
+    if @account.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @account
+    else
+      render :new, status: :unprocessable_entity #Rails7.0からデフォルトでTurboが導入されており、バリデーションのエラーメッセージを表示させるためにはcreateアクションに以下のようにstatus: :unprocessable_entityを追加する必要がある。
+    end
+  end
+
+  def show
+    @account = Account.find(params[:id])
+  end
+
+  private
+
+    def account_params
+      params.require(:account).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,7 @@
 module AccountsHelper
+  def gravatar_for(account, size: 50)
+    gravatar_id = Digest::MD5::hexdigest(account.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: account.name, class: "gravatar")
+  end
 end
